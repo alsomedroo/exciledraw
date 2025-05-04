14f61455b0f2de7d96d32b835a214529189631ad
alsomedroo <dhruvattek@gmail.com>
2025-05-04 19:44:18 +0530

    ws backend defining

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,6 +1,6 @@
 
 import { WebSocketServer } from 'ws';
-import jwt from 'jsonwebtoken';
+import jwt, { JwtPayload } from 'jsonwebtoken';
 import { JWT_SECRET } from './config';
 
 const wss = new WebSocketServer({ port: 8080 });
@@ -8,12 +8,21 @@ const wss = new WebSocketServer({ port: 8080 });
 wss.on('connection', function connection(ws,request) {
   const url = request.url;
   if(!url) {
-    return
+    return;
   }
   const queryParams = new URLSearchParams(url.split('?')[1]);
   const token = queryParams.get('token') || '';  
 
   const decoded = jwt.verify(token, JWT_SECRET)
+
+  if(typeof decoded === 'string') return; // If the token is invalid, decoded will be a string with the error message
+  // If the token is valid, decoded will be an object with the payload
+
+  
+  if(decoded || !(decoded as JwtPayload).userId) {
+    ws.close();
+    return; 
+  }
 
   ws.on('message', function message(data) {
     ws.send('Hello! You sent -> ' + data);
